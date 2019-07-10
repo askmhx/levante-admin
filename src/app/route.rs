649de0bc713cc4447466c774f8bd8f1bc65a0813
Crazz <askmhx@gmail.com>
2019-07-10 15:03:06 +0800

    commit favicon.ico get problem

--- src/app/route.rs
+++ src/app/route.rs
@@ -1,12 +1,13 @@
 use actix_web::web;
 use actix_web::web::{HttpResponse};
 use actix_web::App;
-use actix_files::Files;
+use actix_files::Files as Fs;
 
 use crate::controller;
 
 pub fn routers(cfg: &mut web::ServiceConfig) {
     cfg.service(web::resource("/test").route(web::get().to(|| HttpResponse::Ok())));
-    cfg.service(Files::new("/favicon.ico", "/Users/Crazz/WorkSpace/RSWork/levante-admin/statics/").index_file("index.html"));
+    cfg.service(Fs::new("/favicon.ico", "/Users/Crazz/WorkSpace/RSWork/levante-admin/statics/").index_file("favicon.ico"));
+//    cfg.service(Fs::new("/favicon.ico", "/Users/Crazz/WorkSpace/RSWork/levante-admin/statics/").index_file("index.html"));
 //    cfg.service(App::service(controller::xmlrpc));
 }
